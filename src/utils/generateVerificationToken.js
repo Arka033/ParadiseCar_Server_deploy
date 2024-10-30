ff51f325fa15f0a8c57b4f46d9be0756f70a42fe
mr.user <116547937+Arka033@users.noreply.github.com>
2024-10-30 15:10:27 +0530

    Update generateVerificationToken.js

--- src/utils/generateVerificationToken.js
+++ src/utils/generateVerificationToken.js
@@ -11,14 +11,16 @@ const generateVerificationToken= async (res,userId)=>{
       console.log(isProduction);
       
 
-      await res.cookie("AuthToken", jwtToken, {
-        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-        httpOnly: true,
-        secure: isProduction, 
-        sameSite:  'None', 
-        // domain: isProduction ? 'paradisecar-server-deploy.onrender.com' : 'localhost', // Set domain
-        path: '/',  // Ensure the cookie is available for all routes
-      });
+     await res.cookie("AuthToken", jwtToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000, 
+        httpsOnly: true,                  
+        secure: isProduction,  
+        sameSite:'strict',           
+        // sameSite: isProduction ? 'None' : 'Lax', // Cross-origin in production, restricted in development
+        // domain: isProduction ? 'paradisecar-server-deploy.onrender.com' : 'localhost', 
+        path: '/',                        // Cookie is valid across the entire site
+     });
+     
       
 
 } 
